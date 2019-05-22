2db88ccef9ffcb1621ac16fdd55531001695c7c9
mrich <mrich@kth.se>
2019-05-22 13:34:50 +0200

    Update getStarted.js

--- getStarted.js
+++ getStarted.js
@@ -10,53 +10,70 @@ import { Haptic } from 'expo';
 export default class Start extends React.Component {
   static navigationOptions = {
     title: 'Welcome',
+    headerTransparent: true,
+    headerTintColor: "transparent",
 
   };
-    constructor(props) {
-        super(props);
-        this.state = { text: "" };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
 
-    componentDidMount() {
+  componentDidMount() {
 
-    }
+  }
 
-    pickUserName () {
-      
-    }
+  pickUserName() {
 
-    render() {
-      const { navigate } = this.props.navigation;
-      let startText = "What username do you want?"
+  }
 
-      let start = 
+  render() {
+    const { navigate } = this.props.navigation;
+    let startText = "What username do you want?"
+
+    let start =
       <View>
-        <TouchableOpacity style={{ }} activeOpacity={0.5} onPress={() => {
-          navigate('Home', { username: this.state.text})
+        <TouchableOpacity style={{}} activeOpacity={0.5} onPress={() => {
+          navigate('Home', { username: this.state.text })
         }}>
-        <Text style={{ paddingHorizontal: 15, color: "deepskyblue", fontSize: 20 }}>Sign in</Text>
+          <Text style={{ paddingHorizontal: 15, color: "deepskyblue", fontSize: 20 }}>Sign in</Text>
         </TouchableOpacity>
 
       </View>;
 
-        return <View style={{
-            flex: 1,
-            alignItems: 'center',
+    return (
+      <ImageBackground
+        style={{
+          backgroundColor: '#ccc',
+          flex: 1,
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          justifyContent: 'center',
+        }}
+        source={{ uri: "http://miniwallist.com/wp-content/uploads/2016/07/pokemon-go-pokeball-mobile-wallpaper-minimalist-2160x3840.jpg" }}
+      >
+        <View style={{
+          flex: 1,
+          alignItems: 'center',
         }}>
-        
-        <Image style={{ width: 300, height: 300, resizeMode: "contain"}} source={{ uri: "https://fontmeme.com/permalink/190522/b394043e2419f9781e8e5c25b69fd896.png" }}/>
-        <TextInput
-          style={{height: 40, width: 200, borderColor: 'gray', borderWidth: 2, fontSize: 20}}
-          onChangeText={(new_input) => {
-            this.setState({text: new_input})}}
-          value={this.state.text} maxLength={13} placeholder="Enter username" onSubmitEditing={(event) => { 
-            console.log("inside started: ", event.nativeEvent.text)
-            console.log("from state: ", this.state.text)
-            navigate('Home', { username: event.nativeEvent.text})}}
-        />
-        
-        {start}
 
-        </View>;
-    }
+          <Image style={{ width: 300, height: 300, resizeMode: "contain" }} source={{ uri: "https://fontmeme.com/permalink/190522/b394043e2419f9781e8e5c25b69fd896.png" }} />
+          <TextInput
+            style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 2, fontSize: 20, backgroundColor: "white" }}
+            onChangeText={(new_input) => {
+              this.setState({ text: new_input })
+            }}
+            value={this.state.text} maxLength={13} placeholder="Enter username" onSubmitEditing={(event) => {
+              console.log("inside started: ", event.nativeEvent.text)
+              console.log("from state: ", this.state.text)
+              navigate('Home', { username: event.nativeEvent.text })
+            }}
+          />
+
+          {start}
+
+        </View>
+        </ImageBackground>)
   }
+}
